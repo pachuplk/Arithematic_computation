2c0218c59d2871e79e0168cfa3dc6b00194bc02a
pachuplk <vanguardplk@gmail.com>
2022-05-13 00:04:51 +0530

    input data

---
+++ input.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo " Welcome to Shell Scripting Program: Sorting an Array";
+
+# Take three user input
+read -p "Enter the value of a: " a;
+read -p "Enter the value of b: " b;
+read -p "Enter the value of c: " c;
